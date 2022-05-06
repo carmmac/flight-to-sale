fab7dbc37502ea7742fb6abd5832b657277f9bdd
bremxx <petr.kirienko@gmail.com>
2022-05-06 18:26:51 +0300

    added empty list plug message

--- src/components/flight-list.vue
+++ src/components/flight-list.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="list-wrapper">
     <h2 class="visually-hidden">Список предложений</h2>
-    <ul class="flight-list">
+    <ul v-if="getFlightsPerPage.length > 0" class="flight-list">
       <li class="card" v-for="(flight, i) in getFlightsPerPage" :key="`flight-${i}`">
         <flight-card :route="flight" />
       </li>
     </ul>
+    <div v-else>По Вашему запросу ничего не найдено</div>
     <button v-if="displayLoadMoreButton" class="btn btn--load-more">Показать еще</button>
   </div>
 </template>
